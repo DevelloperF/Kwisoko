7ea3429121ce7af80c838bd22de935e5aeb9eeba
NIYOMUKUNZI Jésus-Marie Filao <niyojesusmarie@gmail.com>
2017-10-03 02:30:37 +0300

    Ugusubira Inyuma Ntibirimwo

---
+++ shoppingbackend/src/main/java/bi/filao_s/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package bi.filao_s.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
